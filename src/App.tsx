d49446f1927b61455178b9456b64ad4050d21a56
Thiago Pizzutti <tpizzutti@gmail.com>
2020-06-23 17:46:20 -0300

    fonts, button styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import GlobalStyle from './styles/global';
+import Button from './components/Button';
 
 const App: React.FC = () => (
-  <GlobalStyle>
-    <div className='App'></div>
-  </GlobalStyle>
+  <Fragment>
+    <h1>Hello Rose</h1>
+    <Button
+      backgroundColor="black"
+      textColor="white"
+      backgroundColorOnHover="white"
+      textColorOnHover="black"
+    >
+      Texto Qualquer
+    </Button>
+
+    <GlobalStyle />
+  </Fragment>
 );
 
 export default App;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,25 +1,28 @@
-import React, { ButtonHTMLAttributes } from "react";
-import { StyledButton } from "./styles";
+import React, { ButtonHTMLAttributes } from 'react';
+import { StyledButton } from './styles';
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  backgroundColorx: string;
+  backgroundColor: string;
   textColor: string;
   backgroundColorOnHover: string;
   textColorOnHover: string;
 }
 
 const Button: React.FC<IProps> = ({
-  backgroundColorx,
+  children,
+  backgroundColor,
   textColor,
   backgroundColorOnHover,
   textColorOnHover,
 }) => (
   <StyledButton
-    backgroundColorx={backgroundColorx}
+    backgroundColor={backgroundColor}
     textColor={textColor}
     backgroundColorOnHover={backgroundColorOnHover}
     textColorOnHover={textColorOnHover}
-  />
+  >
+    {children}
+  </StyledButton>
 );
 
 export default Button;
