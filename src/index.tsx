0d836b7122d3a09c76b79570790301476479292e
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-06-23 18:14:38 -0300

    global variables created in ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  salmon: '#F2D9D0',
+  orange: '#F2BC8D',
+  black: '#384001',
+  white: '#FAFBFC',
+  lightSteelBlue: '#B0C6D9',
+  cornFlowerBlue: '#54AFDC',
+  red: '#D94A64',
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
