d2be6295bb54a3d6655fb321a428b1c0d917a8de
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-25 09:36:22 -0300

    Criar index for components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Button from './Button';
+import HomeCard from './HomeCard';
+import Input from './Input';
+import Modal from './Modal';
+import Navbar from './Navbar';
+
+export default { Button, HomeCard, Input, Modal, Navbar };
