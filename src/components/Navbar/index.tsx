c3052e65401036793c74bfe9d57184a53f2ed8e7
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-06-25 03:07:51 -0300

    feat: add Modal component with styles and animations

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/navbar/logo.svg';
 import magnifier from '../../assets/navbar/magnifier.svg';
 import profile from '../../assets/navbar/profile.svg';
 import menu from '../../assets/navbar/menu.svg';
 
+import Modal from '../../components/Modal';
 import { Container, Nav } from './styles';
 
 const Navbar: React.FC = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <Container>
+      <Modal toggle={toggle} handleToggle={handleToggle} />
       <figure>
         <img src={logo} alt="rose" />
       </figure>
       <Nav>
         <ul>
-          <li>
+          <li onClick={handleToggle}>
             <img src={magnifier} alt="pesquisar" />
           </li>
-          <li>
+          <li onClick={handleToggle}>
             <img src={profile} alt="perfil" />
           </li>
-          <li>
+          <li onClick={handleToggle}>
             <img src={menu} alt="menu" />
           </li>
         </ul>
