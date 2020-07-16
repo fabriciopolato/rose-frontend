7ed650a2c7cee191bea9aa2015a3cbf6f0a21a71
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-16 11:42:15 -0300

    fix: change checkbox from uncontrolled to cotrolled

--- src/components/FeelingCheckbox/index.tsx
+++ src/components/FeelingCheckbox/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
 
 import { Label } from './styles';
 
-interface IProps {
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
 }
 
-const FeelingCheckbox: React.FC<IProps> = ({ children, id }) => {
+const FeelingCheckbox: React.FC<IProps> = ({ children, id, ...rest }) => {
   return (
     <Label htmlFor={id}>
-      <input type="checkbox" id={id} />
+      <input {...rest} type="checkbox" id={id} />
       {children}
     </Label>
   );
--- src/pages/Feeling/index.tsx
+++ src/pages/Feeling/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
 import { Navbar, Footer, FeelingCheckbox, Input, Button } from '../../components';
 import { Container, Content, CardQuestions, CardCheckboxes } from './styles';
-import { useTheme } from 'styled-components'
-
+import { useTheme } from 'styled-components';
 
 const Feeling: React.FC = () => {
+  const [feelings, setFeelings] = useState({
+    ansiedade: false,
+    depressão: false,
+    'baixa_auto-estima': false,
+    relacionamentos_ruins: false,
+    dúvidas_de_identidade: false,
+    orientação: false,
+  });
+
   const { orange, white, lightSteelBlue } = useTheme();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setFeelings({ ...feelings, [name]: checked });
+  };
 
   return (
     <Container>
@@ -16,12 +29,54 @@ const Feeling: React.FC = () => {
           <p>Nos conte um pouco como você está se sentindo:</p>
         </CardQuestions>
         <CardCheckboxes>
-          <FeelingCheckbox id="anxiety">Ansiedade</FeelingCheckbox>
-          <FeelingCheckbox id="depression">Depressão</FeelingCheckbox>
-          <FeelingCheckbox id="low-self-esteem">Baixa auto-estima</FeelingCheckbox>
-          <FeelingCheckbox id="toxic-relationships">Relacionamentos ruins</FeelingCheckbox>
-          <FeelingCheckbox id="identity-trouble">Dúvidas de identidade</FeelingCheckbox>
-          <FeelingCheckbox id="guidance">Orientação </FeelingCheckbox>
+          <FeelingCheckbox
+            onChange={handleChange}
+            checked={feelings.ansiedade}
+            name="ansiedade"
+            id="anxiety"
+          >
+            Ansiedade
+          </FeelingCheckbox>
+          <FeelingCheckbox
+            onChange={handleChange}
+            checked={feelings.depressão}
+            name="depressão"
+            id="depression"
+          >
+            Depressão
+          </FeelingCheckbox>
+          <FeelingCheckbox
+            onChange={handleChange}
+            checked={feelings['baixa_auto-estima']}
+            name="baixa_auto-estima"
+            id="low-self-esteem"
+          >
+            Baixa auto-estima
+          </FeelingCheckbox>
+          <FeelingCheckbox
+            onChange={handleChange}
+            checked={feelings.relacionamentos_ruins}
+            name="relacionamentos_ruins"
+            id="toxic-relationships"
+          >
+            Relacionamentos ruins
+          </FeelingCheckbox>
+          <FeelingCheckbox
+            onChange={handleChange}
+            checked={feelings.dúvidas_de_identidade}
+            name="dúvidas_de_identidade"
+            id="identity-trouble"
+          >
+            Dúvidas de identidade
+          </FeelingCheckbox>
+          <FeelingCheckbox
+            onChange={handleChange}
+            checked={feelings.orientação}
+            name="orientação"
+            id="guidance"
+          >
+            Orientação{' '}
+          </FeelingCheckbox>
           <Input type="text" placeholder="Outro:"></Input>
           <Button
             backgroundColor={orange}
