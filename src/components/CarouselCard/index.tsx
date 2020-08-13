58e0f58d043a84d11ae489825b1eceeb19799a95
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-08-13 10:21:14 -0300

    fix: remove unused asset

--- src/components/CarouselCard/index.tsx
+++ src/components/CarouselCard/index.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { Button } from '..';
 import { Card } from './styles';
 
-import polatinho from '../../assets/polato.jpeg';
 import ratingStars from '../../assets/rating-stars.svg';
 
 import { useTheme } from 'styled-components';
